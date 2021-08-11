e730b06413408589bff11915bf380065c6f57f6d
choro <hisstoryxx@gmail.com>
2021-08-11 16:07:51 +0900

    채팅 구현 1

---
+++ screens/ChatsScreen.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { View } from '../components/Themed';
+import ChatListItem from '../components/ChatListItem';
+
+import ChatRooms from '../data/ChatRooms';
+import { FlatList } from 'react-native-gesture-handler';
+
+
+export default function ChatsScreen() {
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data = {ChatRooms} 
+        renderItem ={({item}) => <ChatListItem chatRoom={item}/>}
+        keyExtractor = {(item) => item.id}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }, 
+});
---
+++ types.tsx
@@ -0,0 +1,42 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type MainTabParamList = {
+  Camera: undefined;
+  Chats: undefined;
+  Status: undefined;
+  Calls: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
+export type User ={
+  id : String;
+  name : String;
+  imageUri: String;
+}
+
+export type Message = {
+  id: String;
+  content: String;
+  createdAt: String;
+}
+
+export type ChatRoom = {
+  id : String;
+  users : [User];
+  lastMessage : Message;
+}
