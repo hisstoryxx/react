d8029c7aca24a7f1a26505a6ff2307241ddaeb65
cho <hisstoryxx@gmail.com>
2021-10-23 15:26:53 +0900

    fetch chat rooms

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,31 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { Text, Image, View, StyleSheet, FlatList, Pressable } from 'react-native';
 import ChatRoomItem from '../components/ChatRoomItem';
-import { Auth } from 'aws-amplify';
+import { Auth, DataStore } from 'aws-amplify';
+import { ChatRoom, ChatRoomUser } from '../src/models';
 import chatRoomsData from '../assets/dummy-data/ChatRooms';
 
 export default function TabOneScreen() {
+
+  const [ chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
+
+  useEffect ( () => {
+    const fetchChatRooms = async () => {
+      const userData = await Auth.currentAuthenticatedUser();
+
+      const chatRooms = (await DataStore.query(ChatRoomUser))
+      .filter(chatRoomUser => chatRoomUser.user.id === userData.attributes.sub)
+      .map(chatRoomUser => chatRoomUser.chatroom );
+      console.log(chatRooms)
+    
+    setChatRooms(chatRooms);
+
+
+    };
+    fetchChatRooms();
+  }, [] );
+
   const logOut = () => {
     Auth.signOut();
 
@@ -14,7 +34,7 @@ export default function TabOneScreen() {
   return (
     <View style={styles.page}>
        <FlatList 
-        data={chatRoomsData}
+        data={chatRooms}
         renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
         showsVerticalScrollIndicator={false}
       />
