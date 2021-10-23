fae67004c99cf93c8ab068f0e4668d49e12b27e6
cho <hisstoryxx@gmail.com>
2021-10-23 18:32:20 +0900

    Messages

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { Text, Image, View, Pressable, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 import { DataStore } from '@aws-amplify/datastore';
-import { ChatRoomUser, User } from '../../src/models';
+import { ChatRoomUser, Message, User } from '../../src/models';
 import styles from './styles';
 import { AWS_CLOUDWATCH_BASE_BUFFER_SIZE } from '@aws-amplify/core';
 import Auth from '@aws-amplify/auth';
@@ -10,10 +10,12 @@ import Auth from '@aws-amplify/auth';
 export default function ChatRoomItem({ chatRoom }) {
   // const [users,setUsers] = useState<User[]>([]); // all users in this chatroom
   const [user, setUser] = useState<User|null>(null); // the display user
+  const [lastMessage, setLastMessage] = useState<Message|undefined>(undefined);
 
-  console.log(chatRoom)
+  
 
   const navigation = useNavigation();
+  
 
   useEffect( () => {
     const fetchUsers = async () => {
@@ -28,7 +30,16 @@ export default function ChatRoomItem({ chatRoom }) {
 
     };
     fetchUsers();
-   },[])
+   },[]);
+
+   useEffect(()=> {
+     if (!chatRoom.chatRoomLastMessageId) {
+       return
+     }
+     DataStore.query(Message, chatRoom.chatRoomLastMessageId).then(setLastMessage);
+   })
+
+   
 
   const onPress = () => {
     
@@ -50,9 +61,9 @@ export default function ChatRoomItem({ chatRoom }) {
       <View style={styles.rightContainer}>
         <View style={styles.row}>
           <Text style={styles.name}>{user.name}</Text>
-          <Text style={styles.text}>{chatRoom.lastMessage?.createdAt}</Text>
+          <Text style={styles.text}>{lastMessage?.createdAt}</Text>
         </View>
-        <Text numberOfLines={1} style={styles.text}>{chatRoom.lastMessage?.content}</Text>
+        <Text numberOfLines={1} style={styles.text}>{lastMessage?.content}</Text>
       </View>
     </Pressable>
   );
--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { DataStore } from '@aws-amplify/datastore';
+import { User } from '../../src/models';
+import Auth from '@aws-amplify/auth';
 
 const blue = '#3777f0';
 const grey = 'lightgrey';
@@ -7,9 +10,29 @@ const grey = 'lightgrey';
 const myID = 'u1';
 
 const Message = ({ message }) => {
+  const [user, setUser] = useState<User|undefined>(undefined);
+  const [isMe, setIsMe] = useState<boolean>(false);
 
-  const isMe = message.user.id === myID;
+  useEffect(( ) => {
+    DataStore.query(User, message.userID).then(setUser);
+  }, []);
 
+  useEffect( () => {
+    const checkIfMe = async () => {
+      if (!user) {
+        return;
+      }
+      const authUser = await Auth.currentAuthenticatedUser();
+      setIsMe(user.id === authUser.attributes.sub)
+    }
+    checkIfMe();
+  },[user])
+
+  if (!user) {
+    return < ActivityIndicator /> 
+  }
+
+  
   return (
     <View style={[styles.container, isMe ? styles.rightContainer : styles.leftContainer]}>
       <Text style={{ color: isMe ? 'black' : 'white'}}>{message.content}</Text>
