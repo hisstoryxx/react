b59c0feba478f6dc3d9779f6c0127ecdb71520ab
cho <hisstoryxx@gmail.com>
2021-10-27 16:13:07 +0900

    emojis

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -12,9 +12,12 @@ import { SimpleLineIcons, Feather, MaterialCommunityIcons, AntDesign, Ionicons }
 import { DataStore } from '@aws-amplify/datastore';
 import { Message, ChatRoom } from '../../src/models';
 import Auth from '@aws-amplify/auth';
+import EmojiSelector from 'react-native-emoji-selector';
 
 const MessageInput = ({chatRoom}) => {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState(false);
+
 
   const sendMessage = async () => {
     // send message
@@ -27,7 +30,8 @@ const MessageInput = ({chatRoom}) => {
 
     updateLastMessage(newMessage);
 
-    setMessage('');
+    setMessage("");
+    setIsEmojiPickerOpen(false);
   }
 
   const updateLastMessage = async (newMessage) => { 
@@ -50,12 +54,24 @@ const MessageInput = ({chatRoom}) => {
 
   return (
     <KeyboardAvoidingView 
-      style={styles.root} 
+      style={[styles.root, {height: isEmojiPickerOpen ? "50%" : "auto"}]} 
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={100}
     >
+      <View style = {styles.row}>
       <View style={styles.inputContainer}>
-        <SimpleLineIcons name="emotsmile" size={24} color="#595959" style={styles.icon} />
+        <Pressable
+          onPress ={ () => setIsEmojiPickerOpen((currentValue) => !currentValue)
+          }
+        >
+          <SimpleLineIcons 
+            name="emotsmile" 
+            size={24} 
+            color="#595959" 
+            style={styles.icon} 
+          />
+        </Pressable>
+        
         
         <TextInput 
           style={styles.input}
@@ -67,17 +83,32 @@ const MessageInput = ({chatRoom}) => {
         <Feather name="camera" size={24} color="#595959" style={styles.icon} />
         <MaterialCommunityIcons name="microphone-outline" size={24} color="#595959" style={styles.icon} />
       </View>
+
+
       <Pressable onPress={onPress} style={styles.buttonContainer}>
         {message ? <Ionicons name="send" size={18} color="white" /> : <AntDesign name="plus" size={24} color="white" />}
       </Pressable>
+
+      </View>
+      
+
+      {isEmojiPickerOpen&& (
+      <EmojiSelector 
+        onEmojiSelected={emoji => setMessage(currentMessage => currentMessage + emoji)} 
+        columns={8}
+      />
+      )}
     </KeyboardAvoidingView>
   )
 }
 
 const styles = StyleSheet.create({
   root: {
-    flexDirection: 'row',
     padding: 10,
+  },
+  row: { 
+    flexDirection: 'row',
+
   },
   inputContainer: {
     backgroundColor: '#f2f2f2',
